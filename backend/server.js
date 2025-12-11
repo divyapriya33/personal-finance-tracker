2d7383476d4c8b81792eab2cbfd5073f8c6adb96
divyapriya33 <priyadivya51234@gmail.com>
2025-12-11 09:15:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import transactionRoutes from './routes/transactions.js';
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/transactions', transactionRoutes);
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
