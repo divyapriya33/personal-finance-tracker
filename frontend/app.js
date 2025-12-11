aae20434ff0e2d4333f3048d24a6afcef326a0d2
divyapriya33 <priyadivya51234@gmail.com>
2025-12-11 09:24:59 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,14 @@
+document.getElementById("loadBtn").addEventListener("click", async () => {
+  const res = await fetch("http://localhost:5000/api/transactions");
+  const data = await res.json();
 
+  const container = document.getElementById("transactions");
+  container.innerHTML = "";
+
+  data.forEach(t => {
+    const div = document.createElement("div");
+    div.className = "item";
+    div.innerText = `${t.type.toUpperCase()}: ₹${t.amount}`;
+    container.appendChild(div);
+  });
+});
